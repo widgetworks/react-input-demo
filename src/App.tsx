617c6b20d3d81ebdc5d79de4d763ef54c0261a39
Coridyn Fitzgerald-Hood <coridyn@gmail.com>
2021-04-29 02:38:57 +1000

    Setup basic application with input

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+    return (
+        <div className="App">
+            <label>
+                <span className="label-text">Input:</span>
+                <input type="text" />
+            </label>
+            
+            <p>
+                Value: { '[display output value here]' }
+            </p>
+        </div>
+    );
+}
+
+export default App;
