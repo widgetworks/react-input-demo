91f5aebc2a128f87f6abce738bb1f818be92be90
Coridyn Fitzgerald-Hood <coridyn@gmail.com>
2021-05-05 10:43:39 +1000

    refactor(app): wire up 'input' element for demo

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Functional component
 
 Write a functional component that wraps a text input element.
-  - the text input shouldn't commit the user's input value immediately (i.e. as the user types)
+  - the text input should not commit the user's input value immediately (i.e. the outside value should not change as the user types)
   - instead, it should commit the value when the user presses enter or leaves the input field
-  - if the user presses the escape key then we should rollback to the last-entered value
+  - if the user presses the escape key then we should rollback to the last "committed" value (i.e. the last value that was in the field before the most recent edits were made).
+
+
+## Live example for reference
+
+For a live example of this editing behaviour, please see this calculator:
+https://wm.widgetworks.com.au/widget/bimade/live
+
+All of the numeric inputs (property value, loan amount, loan term, interest rate) use this behaviour.
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
+import * as React from 'react';
+import { useState } from 'react';
+
 function App() {
+    const [value, setValue] = useState('');
+    
     return (
         <div className="App">
+            <p>
+                <strong>See instructions in `README.md`</strong>
+            </p>
+            
             <label>
                 <span className="label-text">Input:</span>
-                <input type="text" />
+                
+                {/*
+                Replace this `input` element with a component that implements our
+                desired editing behaviour described in the README.md
+                */}
+                <input type="text" value={value} onChange={(e) => {
+                    setValue(e.currentTarget.value);
+                }} />
             </label>
             
             <p>
-                Value: { '[display output value here]' }
+                Value: "{ value }"
             </p>
         </div>
     );
